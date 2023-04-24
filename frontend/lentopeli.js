d7706c38719a195c4bf5e061a543be938bef7e33
ValiMaleki <65647858+ValiMaleki@users.noreply.github.com>
2023-04-24 15:09:05 +0300

    Update lentopeli.js

--- frontend/lentopeli.js
+++ frontend/lentopeli.js
@@ -12,22 +12,6 @@ function getAirports(connection) {
 }
 
 
-
-fetch('/search?name=' + name + '&job=' + job)
-  .then(response => {
-    // Handle the response from the server
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the request
-  });
-function getAirports(connection) {
-    const sql = "SELECT iso_country, ident, name, type, latitude_deg, longitude_deg\n" +
-        "             FROM airport";
-    const cursor = connection.cursor({dictionary: true});
-    cursor.execute(sql);
-    const result = cursor.fetchall();
-    return result;
-}
 // Create an array of addresses
 var addresses = [  {    name: 'Address 1',    latitude: 40.7128,    longitude: -74.0060  },  {    name: 'Address 2',    latitude: 37.7749,    longitude: -122.4194  },  {    name: 'Address 3',    latitude: 51.5074,    longitude: -0.1278  },  {    name: 'Address 4',    latitude: 48.8566,    longitude: 2.3522  },  {    name: 'Address 5',    latitude: 35.6895,    longitude: 139.6917  }];
 
