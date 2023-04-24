5e7fffce425fbd67f5bdad7b9cf720394e25d120
ValiMaleki <65647858+ValiMaleki@users.noreply.github.com>
2023-04-24 15:07:44 +0300

    Update lentopeli.js

--- frontend/lentopeli.js
+++ frontend/lentopeli.js
@@ -2,6 +2,17 @@ let name = prompt("Please enter your name:");
 let job = prompt("Please enter your job:");
 
 
+function getAirports(connection) {
+    const sql = "SELECT iso_country, ident, name, type, latitude_deg, longitude_deg\n" +
+        "             FROM airport";
+    const cursor = connection.cursor({dictionary: true});
+    cursor.execute(sql);
+    const result = cursor.fetchall();
+    return result;
+}
+
+
+
 fetch('/search?name=' + name + '&job=' + job)
   .then(response => {
     // Handle the response from the server
@@ -9,19 +20,32 @@ fetch('/search?name=' + name + '&job=' + job)
   .catch(error => {
     // Handle any errors that occurred during the request
   });
+function getAirports(connection) {
+    const sql = "SELECT iso_country, ident, name, type, latitude_deg, longitude_deg\n" +
+        "             FROM airport";
+    const cursor = connection.cursor({dictionary: true});
+    cursor.execute(sql);
+    const result = cursor.fetchall();
+    return result;
+}
+// Create an array of addresses
+var addresses = [  {    name: 'Address 1',    latitude: 40.7128,    longitude: -74.0060  },  {    name: 'Address 2',    latitude: 37.7749,    longitude: -122.4194  },  {    name: 'Address 3',    latitude: 51.5074,    longitude: -0.1278  },  {    name: 'Address 4',    latitude: 48.8566,    longitude: 2.3522  },  {    name: 'Address 5',    latitude: 35.6895,    longitude: 139.6917  }];
 
+// Create a Leaflet map
+var map = L.map('map').setView([40.7128, -74.0060], 10);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Add a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+// Loop through the addresses array and add markers to the map
+addresses.forEach(function(address) {
+  L.marker([address.latitude, address.longitude]).addTo(map)
+    .bindPopup(address.name);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+});
+
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
@@ -29,5 +53,4 @@ var circle = L.circle([51.508, -0.11], {
     radius: 500
 }).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
