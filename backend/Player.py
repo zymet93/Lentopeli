2645728c444e6e49ebc667a9bea0e764b8823c6c
royk <royk@metropolia.fi>
2023-04-26 12:01:30 +0300

    Implement flying between airports

--- backend/Player.py
+++ backend/Player.py
@@ -1,5 +1,5 @@
 class Player:
-    _WORK_AMOUNT = 3
+    _WORK_AMOUNT = 1
 
     def __init__(self, id, location, time, money, a_ports, profession, name):
         self.id = id
@@ -10,13 +10,8 @@ class Player:
         self.a_ports = a_ports
         self.resetWork()
         self.canShuffleWork = 1
-        self.worked_in_country = False
         self.name = name
 
     def resetWork(self):
         self.canShuffleWork = 1
         self.canWorkAmt = self._WORK_AMOUNT
-
-    def getLocation(self):
-        sql = "select ident, airport.name as 'airport_name', country.name as 'country_name', country.iso_country from airport, country where airport.iso_country=country.iso_country and ident='" + self.location + "'";
-        return execute_sql(sql)[0]
