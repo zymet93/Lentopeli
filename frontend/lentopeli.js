d61481cf1a58567264dff5baa40af063b4548f7b
royk <royk@metropolia.fi>
2023-04-27 17:50:51 +0300

    Fix broken updating of map Inhibit flying to visited countries

--- frontend/lentopeli.js
+++ frontend/lentopeli.js
@@ -19,6 +19,80 @@ function resetcookie() {
     document.cookie = "fgplayercookie=;expires=Thu, 01 Jan 1970 00:00:01 GMT";
 }
 
+function flytobotan(target) {
+    const cookieValue = document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
+    fetch("http://127.0.0.1:3000/fly/" + cookieValue + "/" + target).then(function(){window.location.reload(false)});
+}
+
+// Function to add markers from API data to the map
+async function addMarkers() {
+
+    // Create a Leaflet map and set the initial view to somewhere in Europe
+    var map = L.map('map').setView([48.8566, 2.3522], 5);
+
+    // Add a tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+
+    let mycookieValue = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
+    const val = await fetch("http://127.0.0.1:3000/airports/" + mycookieValue);
+    const jsonData = await val.json();
+
+    const countrySelect = document.getElementById('country-select');
+    for (let i = 0; i < jsonData.length; i++) {
+        const marker = L.marker([jsonData[i].latitude_deg, jsonData[i].longitude_deg]).addTo(map);
+        marker.bindPopup(`${jsonData[i].name} (${jsonData[i].ident}) - ${jsonData[i].iso_country}`);
+        marker.on('click', function() {
+            const answer = confirm(`Do you want to fly to ${jsonData[i].name}?`);
+            if (answer) {
+                const selectedOption = countrySelect.querySelector(`option[value='${jsonData[i].ident}']`);
+                if (selectedOption) {
+                    selectedOption.selected = true;
+                    flytobotan(selectedOption.value);
+                }
+                //document.getElementById('fly-button').disabled = false;
+            }
+        });
+    }
+}
+
+
+async function getmyairports() {
+    const val = await fetch("http://127.0.0.1:3000/airports");
+    const jsonData = await val.json();
+    const select = document.getElementById("country-select");
+    for (let i = 0; i < jsonData.length; i++) {
+        const option = document.createElement("option");
+        option.value = jsonData[i].ident;
+        option.text = `${jsonData[i].name} (${jsonData[i].ident}) - ${jsonData[i].iso_country}`;
+        select.add(option);
+    }
+    document.getElementById("country-select").value=document.getElementById("player-toplocation").innerHTML;
+}
+
+function createNewGame() {
+    // Delete saved cookies
+    document.cookie = "fgplayercookie=;expires=Thu, 01 Jan 1970 00:00:01 GMT";
+
+    // Call creategame() function
+    fetch('/player/create/name/job')
+        .then(response => response.json())
+        .then(data => {
+            // Do something with the response data, such as redirecting to a new page
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            location.reload();
+
+        });
+}
+
 if (document.cookie.split(";").some((item) => item.trim().startsWith("fgplayercookie="))) {
     document.getElementById("newdiv").classList.add("hidden");
     const cookieValue = document.cookie
@@ -39,6 +113,30 @@ if (document.cookie.split(";").some((item) => item.trim().startsWith("fgplayerco
 
         select = document.getElementsByClassName("profession")[0];
         select.innerHTML = 'Profession: ' + res.playerProfession + '<progress id="player-profession"></progress>';
+    }).then(function() {
+
+        // Call the function to add markers to the map
+        addMarkers();
+
+        // Call the function to populate the select element
+        getmyairports();
+
+        const flyform = document.querySelector(".country-select-form");
+        flyform.addEventListener("submit", async function(e) {
+            e.preventDefault();
+            const flyselect = document.getElementById("country-select");
+            flytobotan(flyselect.value);
+        });
+
+        let mycookieValue = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
+
+        fetch("http://127.0.0.1:3000/airports/" + mycookieValue)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+
     });
 } else {
     document.getElementById("maindiv").classList.add("hidden");
@@ -69,99 +167,3 @@ plyform.addEventListener("submit", async function(e) {
         window.location.reload(false)
     });
 });
-
-function flytobotan(target) {
-    const cookieValue = document.cookie
-          .split("; ")
-          .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
-    fetch("http://127.0.0.1:3000/fly/" + cookieValue + "/" + target).then(function(){window.location.reload(false)});
-}
-
-// Create a Leaflet map and set the initial view to somewhere in Europe
-var map = L.map('map').setView([48.8566, 2.3522], 5);
-
-// Add a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; OpenStreetMap contributors'
-}).addTo(map);
-
-// Function to add markers from API data to the map
-async function addMarkers() {
-  let mycookieValue = document.cookie
-        .split("; ")
-        .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
-  const val = await fetch("http://127.0.0.1:3000/airports");
-  const jsonData = await val.json();
-
-  for (let i = 0; i < jsonData.length; i++) {
-    const marker = L.marker([jsonData[i].latitude_deg, jsonData[i].longitude_deg]).addTo(map);
-    marker.bindPopup(`${jsonData[i].name} (${jsonData[i].ident}) - ${jsonData[i].iso_country}`);
-    marker.on('click', function() {
-      const answer = confirm(`Do you want to fly to ${jsonData[i].name}?`);
-      if (answer) {
-        const selectedOption = countrySelect.querySelector(`option[value='${jsonData[i].ident}']`);
-        if (selectedOption) {
-          selectedOption.selected = true;
-          flytobotan(selectedOption.value);
-        }
-        document.getElementById('fly-button').disabled = false;
-      }
-    });
-  }
-}
-
-
-// Call the function to add markers to the map
-addMarkers();
-
-
-const countrySelect = document.getElementById('country-select');
-
-async function getmyairports() {
-    let mycookieValue = document.cookie
-          .split("; ")
-          .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
-    const val = await fetch("http://127.0.0.1:3000/airports");
-    const jsonData = await val.json();
-    const select = document.getElementById("country-select");
-    for (let i = 0; i < jsonData.length; i++) {
-        const option = document.createElement("option");
-        option.value = jsonData[i].ident;
-        option.text = `${jsonData[i].name} (${jsonData[i].ident}) - ${jsonData[i].iso_country}`;
-        select.add(option);
-    }
-    document.getElementById("country-select").value=document.getElementById("player-toplocation").innerHTML;
-}
-
-// Call the function to populate the select element
-getmyairports();
-
-const flyform = document.querySelector(".country-select-form");
-flyform.addEventListener("submit", async function(e) {
-    e.preventDefault();
-    const flyselect = document.getElementById("country-select");
-    flytobotan(flyselect.value);
-});
-function createNewGame() {
-    // Delete saved cookies
-    document.cookie = "fgplayercookie=;expires=Thu, 01 Jan 1970 00:00:01 GMT";
-
-    // Call creategame() function
-    fetch('/player/create/name/job')
-    .then(response => response.json())
-    .then(data => {
-        // Do something with the response data, such as redirecting to a new page
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        location.reload();
-
-    });
-}
-let mycookieValue = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("fgplayercookie="))?.split("=")[1];
-fetch("http://127.0.0.1:3000/airports")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
